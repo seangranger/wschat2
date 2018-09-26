a7b3192a67b55ed94fa61a895b0b83327775c024
nowatewat <nowatewat@gmail.com>
2018-09-26 10:55:27 -0600

    server now adds sockets to obj to distinguish between them

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var ws = require('ws');
+var crypto = require('crypto');
 var server = new ws.Server({
     host: '127.0.0.1',
     port: 5000
 });
+var clientsockets = {};
+
 server.on('connection',function(socket){
-  console.log('============someone connected===============')
+  console.log('============someone connected===============');
+  //crypto.randomBytes returns a buffer ---- https://nodejs.org/api/buffer.html
+  var uniquesocketid = crypto.randomBytes(7).toString('hex');
+  clientsockets[uniquesocketid] = {};
+  clientsockets[uniquesocketid].socket = socket;
+  clientsockets[uniquesocketid].sockid = uniquesocketid;
+  clientsockets[uniquesocketid].handle = uniquesocketid;
+  console.log(clientsockets)
 });
 
 
